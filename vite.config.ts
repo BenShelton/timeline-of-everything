1f558a146e5b8d6b9d94caef21240e1972c0c3be
BenShelton <bensheltonjones@gmail.com>
2021-10-25 20:30:20 +0100

    add resolution aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
+import path from 'path'
 import vue from '@vitejs/plugin-vue'
 
+function resolve (...dirs: string[]): string {
+  return path.resolve(__dirname, ...dirs)
+}
+
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve('src')
+    }
+  },
   plugins: [
     vue()
   ]
