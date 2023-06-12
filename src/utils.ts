d769ad32e4093a6c1766cf37e3d299b22bbb8aed
BenShelton <bensheltonjones@gmail.com>
2023-06-12 12:43:26 +0100

    fix negative years off by one

--- src/utils.ts
+++ src/utils.ts
@@ -13,9 +13,12 @@ export function link (href: string): string {
 }
 
 export function prettyDate (date: DateType): string {
-  const d = new Date(date)
-  const str = d.toLocaleDateString()
-  return str.startsWith('01/01/')
-    ? (str.split('/').pop() ?? '')
-    : str
+  const parsedDate = new Date(date)
+  const dateParts = parsedDate.toISOString().split('T')[0].split('-')
+  const [y, m, d] = !dateParts[0] ? dateParts.slice(1) : dateParts
+  let output = ''
+  if (d !== '01') output += d + '-'
+  if (m !== '01') output += m + '-'
+  output += parsedDate.getFullYear() < 0 ? String(Number(y)) : y
+  return output
 }
